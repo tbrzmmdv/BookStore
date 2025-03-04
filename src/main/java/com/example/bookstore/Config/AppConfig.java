597622227ad6a6a3d086afd50c56a42454e1f220
Tabriz Mammadov <tebrizrza05@gmail.com>
2025-03-04 20:38:35 +0400

    MentorLesson Config Update for code

--- src/main/java/com/example/bookstore/Config/AppConfig.java
+++ src/main/java/com/example/bookstore/Config/AppConfig.java
@@ -1,11 +1,20 @@
 package com.example.bookstore.Config;
 
+import com.example.bookstore.Repository.AnotherBookRepository;
 import com.example.bookstore.Repository.BookRepository;
 import com.example.bookstore.Repository.MainRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    BookRepository bookRepository=new BookRepository();
+    @Bean
+    public BookRepository bookRepository(){
+        return new BookRepository();
+    }
+    @Bean
+    public AnotherBookRepository anotherBookRepository(){
+        return new AnotherBookRepository();
+    }
 
 }
